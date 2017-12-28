fc5baccfabfd49974d2e8231aa23ac48bd2ec96c
Justin Winter <justinlevi@gmail.com>
2017-12-28 15:13:58 -0500

    Working Mutation w/ String

--- src/App.js
+++ src/App.js
@@ -1,13 +1,90 @@
 import React, { Component } from 'react';
 
 
-  render() {
-    return (
-      <div>
-        HELLO WORLD
-      </div>
-    );
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+import RecursiveIterator from 'recursive-iterator';
+import objectPath from 'object-path';
+
+
+class App extends Component {
+
+  // state = {
+  //     csrfToken: '',
+  //     oauthToken: {}
+  // };
+
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(target){
+
+    const formData  = new FormData();
+    
+    // search for File objects on the request and set it as formData
+    for(let { node, path } of new RecursiveIterator(target.variables)) {
+        if (node instanceof File) {
+            const id = Math.random().toString(36);
+            formData.append(id, node);
+            objectPath.set(target.variables, path.join('.'), id);
+        }
+    }
+    // Display the key/value pairs
+    // for (var pair of formData.entries()) {
+    //   console.log(pair);
+    // }
+
+    // 
+    // this.props.mutate({ variables: { file: target} })
+    this.props.mutate({ 
+        variables: { 
+          input : {
+            file: target.files[0].name,
+            // files: target.files[0]
+          } 
+        }
+      }
+    ).then(response => {
+      console.log(response)
+    })
+  }
+
+  render = () => {
+    return ( 
+        <input
+          type="file"
+          required
+          onChange={({ target }) => {
+            target.validity.valid && this.handleChange(target)
+            }
+          }
+        />
+    )
+  }
+
 }
 
-export default App;
+
+const submitFile = gql`
+  mutation($input: FileInput!) {
+    uploadFile(input: $input)
+  }
+`;
+
+// const submitFile = gql`
+//   mutation($file: FileInput!) {
+//     uploadFile(input: $file){
+//       entity{
+//         ...on FileFile {
+//           url
+//         }
+//       }
+//     }
+//   }
+// `;
+
+export default graphql(submitFile)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,75 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import axios from 'axios';
+import Querystring from 'query-string';
+// import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from 'apollo-client'
+import { createUploadLink } from "apollo-upload-client/lib/main";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+const URL = 'http://contenta.loc';
+const CONSUMER_CREDENTIALS = {
+  grant_type: 'password',
+  client_id: '90f0c0b1-ec7f-47de-a649-454a96e238ab',
+  client_secret: 'test1123',
+  username: 'test',
+  password: 'test'
+};
+
+const initializeCsrfToken = () => {
+  return axios.get(URL + '/session/token')
+    .then(response => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log('error ' + error);
+      return null;
+    });
+};
+
+const initializeOauthToken = () => {
+  return axios.post(URL + '/oauth/token', Querystring.stringify(CONSUMER_CREDENTIALS))
+    .then(response => {
+      return 'Bearer '.concat(response.data.access_token);
+    })
+    .catch((error) => {
+      console.log('error ' + error);
+      return null;
+    });
+};
+
+const initializeHeaders = () => {
+  const headers = {
+    'X-CSRF-Token': initializeCsrfToken().then(
+      (response) => { console.log(response); return response }
+    ).catch((error) => {
+      console.log('error ' + error);
+      return null;
+    }),
+    authorization: initializeOauthToken().then(
+      (response) => { console.log(response); return response }
+    ).catch((error) => {
+      console.log('error ' + error);
+      return null;
+    })
+  };
+  console.log(headers);
+  return headers;
+}
+
+const client = new ApolloClient({
+  // link: new HttpLink({ uri: URL.concat('/graphql') }),
+  link: createUploadLink({
+    uri: URL.concat('/graphql?XDEBUG_SESSION_START=PHPSTORM'),
+    headers: initializeHeaders(),
+  }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'));
 registerServiceWorker();
